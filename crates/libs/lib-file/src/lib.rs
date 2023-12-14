c2aa2514d8bac1b0492bca99112375ac1b52dcb0
Guilhem Mizrahi <guilhem.mizrahi@gmail.com>
2023-12-14 16:35:32 +0100

    Added the hashmap method to find chunks more easily.

--- crates/libs/lib-file/src/lib.rs
+++ crates/libs/lib-file/src/lib.rs
@@ -4,7 +4,7 @@ pub mod mk_fs {
     use anyhow::{bail, Context, Result};
     use log::{debug, error, info, warn};
     use sha2::{Digest, Sha256};
-    use std::{fmt, fs, path::PathBuf};
+    use std::{collections::HashMap, fmt, fs, path::PathBuf};
 
     /// Merkle tree node type enum.
     ///
@@ -248,6 +248,25 @@ pub mod mk_fs {
                 // data: None,
             }));
         }
+
+        /// Create a hashmap linking all `MktFsNode` to its hash.
+        ///
+        /// This method will recursively traverse the Merkle tree to build a flat hashmap referencing the nodes by their hash.
+        pub fn to_hashmap(&self) -> HashMap<[u8; 32], &MktFsNode> {
+            let mut hmap = HashMap::from([(self.hash, self)]);
+
+            match &self.children {
+                None => {
+                    return hmap;
+                }
+                Some(children) => {
+                    for c in children.iter() {
+                        hmap.extend(c.to_hashmap());
+                    }
+                }
+            }
+            return hmap;
+        }
     }
 
     pub fn hash_bytes(bytes: &[u8]) -> [u8; 32] {
@@ -276,77 +295,94 @@ pub mod mk_fs {
         hash.copy_from_slice(hasher.finalize().as_slice());
         return hash;
     }
+}
 
-    #[cfg(test)]
-    mod tests {
+#[cfg(test)]
+mod tests {
 
-        use super::*;
-        use hex;
+    use crate::mk_fs::*;
+    use hex;
+    use std::path::PathBuf;
 
-        #[test]
-        fn lib_file_hash_bytes() {
-            let hash = hash_bytes(b"hello");
-            assert_eq!(
-                hash,
-                hex::decode("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824")
-                    .unwrap()[0..32]
-            );
-        }
+    #[test]
+    fn lib_file_hash_bytes() {
+        let hash = hash_bytes(b"hello");
+        assert_eq!(
+            hash,
+            hex::decode("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824")
+                .unwrap()[0..32]
+        );
+    }
 
-        #[test]
-        fn lib_file_hash_array() {
-            let hash = hash_bytes_array(Vec::from([b"hello".to_vec(), b"world".to_vec()]));
-            assert_eq!(
-                hash,
-                hex::decode("7305db9b2abccd706c256db3d97e5ff48d677cfe4d3a5904afb7da0e3950e1e2")
-                    .unwrap()[0..32]
-            );
-        }
+    #[test]
+    fn lib_file_hash_array() {
+        let hash = hash_bytes_array(Vec::from([b"hello".to_vec(), b"world".to_vec()]));
+        assert_eq!(
+            hash,
+            hex::decode("7305db9b2abccd706c256db3d97e5ff48d677cfe4d3a5904afb7da0e3950e1e2")
+                .unwrap()[0..32]
+        );
+    }
 
-        #[test]
-        fn lib_file_node_from_small_chunk() {
-            let data = b"abc";
-            let path = PathBuf::from("/root");
-            let node = MktFsNode::try_from_bytes(&path, data, 4, 2).unwrap();
-            assert_eq!(
-                node.hash,
-                [
-                    96, 159, 110, 54, 210, 64, 85, 133, 24, 141, 92, 253, 118, 31, 64, 124, 124,
-                    196, 106, 125, 63, 49, 76, 136, 39, 4, 105, 221, 227, 21, 252, 209
-                ]
-            )
-        }
+    #[test]
+    fn lib_file_node_from_small_chunk() {
+        let data = b"abc";
+        let path = PathBuf::from("/root");
+        let node = MktFsNode::try_from_bytes(&path, data, 4, 2).unwrap();
+        assert_eq!(
+            node.hash,
+            [
+                96, 159, 110, 54, 210, 64, 85, 133, 24, 141, 92, 253, 118, 31, 64, 124, 124, 196,
+                106, 125, 63, 49, 76, 136, 39, 4, 105, 221, 227, 21, 252, 209
+            ]
+        )
+    }
 
-        #[test]
-        fn lib_file_node_from_big_chunk() {
-            //           | 4 | 4 | 4 | 4 |2|
-            //           |       |       | |
-            //           |               | |
-            //           |                 |
-            let data = b"abcdefghijklmabcde";
-            let path = PathBuf::from("/root");
-            let node = MktFsNode::try_from_bytes(&path, data, 4, 2).unwrap();
-            assert_eq!(
-                node.hash,
-                [
-                    34, 51, 8, 184, 150, 210, 92, 191, 251, 19, 222, 157, 179, 159, 210, 155, 130,
-                    30, 98, 251, 142, 55, 111, 159, 195, 146, 118, 194, 231, 240, 11, 170
-                ]
-            );
-        }
+    #[test]
+    fn lib_file_node_from_big_chunk() {
+        //           | 4 | 4 | 4 | 4 |2|
+        //           |       |       | |
+        //           |               | |
+        //           |                 |
+        let data = b"abcdefghijklmabcde";
+        let path = PathBuf::from("/root");
+        let node = MktFsNode::try_from_bytes(&path, data, 4, 2).unwrap();
+        assert_eq!(
+            node.hash,
+            [
+                34, 51, 8, 184, 150, 210, 92, 191, 251, 19, 222, 157, 179, 159, 210, 155, 130, 30,
+                98, 251, 142, 55, 111, 159, 195, 146, 118, 194, 231, 240, 11, 170
+            ]
+        );
+    }
 
-        #[test]
-        #[should_panic]
-        fn lib_file_node_from_path_fail() {
-            let path = PathBuf::from("/var/lib/libvirt/images");
-            let read_dir = MktFsNode::try_from_path(&path, 2, 2).unwrap();
-        }
+    #[test]
+    #[should_panic]
+    fn lib_file_node_from_path_fail() {
+        let path = PathBuf::from("/var/lib/libvirt/images");
+        let read_dir = MktFsNode::try_from_path(&path, 2, 2).unwrap();
+    }
 
-        #[test]
-        fn lib_file_node_from_path() {
-            let path = PathBuf::from("./");
-            let node = MktFsNode::try_from_path(&path, 1024, 32).unwrap();
-            // println!("{node}");
+    #[test]
+    fn lib_file_node_from_path() {
+        let path = PathBuf::from("/tmp/abc");
+        let node = MktFsNode::try_from_path(&path, 1024, 32).unwrap();
+        // println!("{node}");
+    }
+
+    #[test]
+    fn lib_file_node_to_hashmap() {
+        let path = PathBuf::from("/tmp/abc");
+        let node = MktFsNode::try_from_path(&path, 1024, 32).unwrap();
+        let map = node.to_hashmap();
+        let hash = [
+            21, 208, 20, 204, 73, 14, 59, 170, 131, 72, 237, 69, 189, 154, 4, 38, 76, 225, 124,
+            157, 143, 164, 78, 78, 6, 169, 69, 111, 79, 182, 40, 196,
+        ];
+        let result = map.get(&hash);
+        match result {
+            Some(r) => println!("{:?}", r),
+            None => println!("Node not found for hash {:?}", hash),
         }
     }
 }
